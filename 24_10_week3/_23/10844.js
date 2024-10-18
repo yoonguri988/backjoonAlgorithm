033e90cc65c25801acffc2356a56110dc0b4ebbd
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-19 00:04:58 +0900

    Update 10844.js and 10844.txt
    Fixed the issue of 10844.js and 10844.txt
    However, I referred to someone else's problem solving.

--- 24_10_week3/_23/10844.js
+++ 24_10_week3/_23/10844.js
@@ -12,4 +12,26 @@ const readInput = (addr) => {
 };
 
 const ip = readInput("10844.txt");
-let op = "";
+
+const N = Number(ip);
+const MOD = 1000000000;
+const dp = Array.from(Array(N + 1), () => Array(10).fill(0));
+dp[1].forEach((v, i, arr) => {
+  if (i > 0) arr[i] = 1;
+});
+
+for (let i = 2; i < dp.length; i++) {
+  dp[i][0] = dp[i - 1][1] % MOD;
+  for (let j = 1; j < 9; j++) {
+    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+    dp[i][j] %= MOD;
+  }
+  dp[i][9] = dp[i - 1][8] % MOD;
+  // console.log(`dp[${i}]: ${dp[i].join("|")}`);
+}
+
+let sum = 0;
+for (let i = 0; i < dp[N].length; i++) {
+  sum += dp[N][i];
+}
+console.log(sum % MOD);
