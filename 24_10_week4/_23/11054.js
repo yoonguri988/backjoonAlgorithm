587d43d7e148138141a745a4a2651ee818a458f7
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-21 17:19:12 +0900

    Create 11054's js file and text file

---
+++ 24_10_week4/_23/11054.js
@@ -0,0 +1,14 @@
+/** 11053 가장 긴 증가하는 부분 수열 */
+const readInput = (addr) => {
+  const fs = require("fs");
+  if (process.platform == "linux") addr = "/dev/stdin";
+  else addr = "./24_10_week3/_23/ip/" + addr;
+  try {
+    const data = fs.readFileSync(addr, "utf-8").toString().trim();
+    return data;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+const ip = readInput("11053.txt");
