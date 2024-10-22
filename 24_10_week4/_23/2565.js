c28220260e30689bc68bdff0d1ecf50d10ed13b8
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-22 14:54:20 +0900

    Create 2565's js file and text file

---
+++ 24_10_week4/_23/2565.js
@@ -0,0 +1,14 @@
+/** 2565 전깃줄 */
+const readInput = (addr) => {
+  const fs = require("fs");
+  if (process.platform == "linux") addr = "/dev/stdin";
+  else addr = "./24_10_week4/_23/ip/" + addr;
+  try {
+    const data = fs.readFileSync(addr, "utf-8").toString().trim();
+    return data;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+const ip = readInput("2565.txt");
