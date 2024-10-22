bb53cefca6ce54d72075df5631ca825a64d2a343
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-22 16:00:40 +0900

    Solve within 1 hout, but 2565 is failed

--- 24_10_week4/_23/2565.js
+++ 24_10_week4/_23/2565.js
@@ -12,3 +12,42 @@ const readInput = (addr) => {
 };
 
 const ip = readInput("2565.txt");
+let [N, ...electLines] = ip.split("\r\n");
+
+let maxLen = 0;
+N = Number(N);
+electLines.forEach((v, i, arr) => {
+  arr[i] = v.split(" ").map(Number);
+  if (maxLen < Math.max(...arr[i])) maxLen = Math.max(...arr[i]);
+});
+
+const start = Array(N);
+const end = Array(N);
+for (let i = 0; i < N; i++) {
+  [start[i], end[i]] = electLines[i];
+}
+// console.log(`start = ${start.join("|")}`);
+// console.log(`end = ${end.join("|")}`);
+const dp = Array(N).fill(0);
+dp[0] = 1;
+
+for (let i = 1; i < N; i++) {
+  let max = 0;
+  for (let j = 0; j < N; j++) {
+    if (start[i] < start[j] && end[i] > end[j]) {
+      max = Math.max(max, dp[j] + 1);
+    }
+  }
+  dp[i] = max;
+}
+console.log(`${dp.join("|")}`);
+
+// for (let i = N - 1; i >= 0; i--) {
+//   let max = 0;
+//   for (let j = i + 1; j < N; j++) {
+//     if (start[i] < start[j] && end[i] < end[j]) {
+//       max = Math.max(max, dp[j] + 1);
+//     }
+//   }
+//   dp[i] = max;
+// }
