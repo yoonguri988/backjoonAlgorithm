6ff5ec8149fb4b8aa22be3ff3083c7e6d7dca4f4
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-22 16:17:10 +0900

    Look at the solution 2565

--- 24_10_week4/_23/2565.js
+++ 24_10_week4/_23/2565.js
@@ -13,41 +13,20 @@ const readInput = (addr) => {
 
 const ip = readInput("2565.txt");
 let [N, ...electLines] = ip.split("\r\n");
-
-let maxLen = 0;
 N = Number(N);
-electLines.forEach((v, i, arr) => {
-  arr[i] = v.split(" ").map(Number);
-  if (maxLen < Math.max(...arr[i])) maxLen = Math.max(...arr[i]);
-});
+electLines = electLines.map((v) => v.split(" ").map(Number));
 
-const start = Array(N);
-const end = Array(N);
-for (let i = 0; i < N; i++) {
-  [start[i], end[i]] = electLines[i];
-}
-// console.log(`start = ${start.join("|")}`);
-// console.log(`end = ${end.join("|")}`);
-const dp = Array(N).fill(0);
-dp[0] = 1;
+const dp = Array(N).fill(1);
+
+electLines.sort((a, b) => a[0] - b[0]);
 
 for (let i = 1; i < N; i++) {
   let max = 0;
-  for (let j = 0; j < N; j++) {
-    if (start[i] < start[j] && end[i] > end[j]) {
-      max = Math.max(max, dp[j] + 1);
+  for (let j = 0; j < i; j++) {
+    if (electLines[i][1] > electLines[j][1]) {
+      max = Math.max(max, dp[j]);
     }
   }
-  dp[i] = max;
+  dp[i] = max + 1;
 }
-console.log(`${dp.join("|")}`);
-
-// for (let i = N - 1; i >= 0; i--) {
-//   let max = 0;
-//   for (let j = i + 1; j < N; j++) {
-//     if (start[i] < start[j] && end[i] < end[j]) {
-//       max = Math.max(max, dp[j] + 1);
-//     }
-//   }
-//   dp[i] = max;
-// }
+console.log(`${N - Math.max(...dp)}`);
