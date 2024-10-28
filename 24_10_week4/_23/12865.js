dd36e220985f969adc71cf4b7d32554de3e2bb3e
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-28 14:15:34 +0900

    Fix: Update solution 12865

--- 24_10_week4/_23/12865.js
+++ 24_10_week4/_23/12865.js
@@ -19,34 +19,22 @@ const products = [];
 for (let i = 0; i < arr.length; i++) {
   products.push(arr[i].split(" ").map(Number));
 }
-// weight가 같으면 value가 높은 것부터
+
 products.sort((a, b) => {
   if (a[1] == b[1]) return a[0] - b[0];
   return b[1] - a[1];
 });
+products.unshift(undefined);
 
-const dp = Array.from(Array(N), () => Array(2).fill(0));
-let max = 0;
-for (let i = 0; i < N; i++) {
-  dp[i] = [...products[i]];
-  // console.log(`dp[${i}]: ${dp[i]}`);
-  for (let j = i + 1; j < N; j++) {
-    // console.log(`j: products [${products[j].join("|")}]`);
-    if (dp[i][0] + products[j][0] > K) continue;
-    else {
-      dp[i][0] = Math.max(dp[i][0], dp[i][0] + products[j][0]);
-      dp[i][1] = Math.max(dp[i][1], dp[i][1] + products[j][1]);
+const dp = Array.from(Array(N + 1), () => Array(K + 1).fill(0));
+for (let i = 1; i <= N; i++) {
+  const [w, v] = products[i];
+  for (let j = 0; j <= K; j++) {
+    if (j < w) {
+      dp[i][j] = dp[i - 1][j];
+    } else {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
     }
   }
-  if (dp[i][0] == K) max = Math.max(max, dp[i][1]);
 }
-console.log(max);
-
-console.log("---- test ----");
-console.log(`${N} ${K}`);
-for (let pd of products) {
-  console.log(`${pd}`);
-}
-for (let i in dp) {
-  console.log(`${dp[i].join("|")}`);
-}
+console.log(dp[N][K]);
