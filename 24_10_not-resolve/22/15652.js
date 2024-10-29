2443433bbeac63d63b88e9f75a9c730281411adb
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-29 14:32:16 +0900

    Feat: Resolve not-resovle list by Number 22
    Create list in 24_10_week1/_22

---
+++ 24_10_not-resolve/22/15652.js
@@ -0,0 +1,17 @@
+function inputTextFile(addr) {
+  const fs = require("fs");
+  if (process.platform == "linux") addr = "/dev/stdin";
+  else addr = "./24_10_not-resolve/inputTextFile/" + addr;
+  try {
+    const data = fs.readFileSync(addr, "utf-8").toString().trim();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const inputData = inputTextFile("15652.txt");
+const outputData = solution();
+// console.log(outputData);
+
+function solution() {}
