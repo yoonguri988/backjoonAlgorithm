e8d54e2f06cb66766dd572a516440096c5dd61c2
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-31 14:32:03 +0900

    Feat: Resolve not-resovle list by Number 22
    Create Soultion 14888 and 14889

---
+++ 24_10_not-resolve/22/14889.js
@@ -0,0 +1,13 @@
+function inputTextFile(addr) {
+  const fs = require("fs");
+  if (process.platform == "linux") addr = "/dev/stdin";
+  else addr = "./24_10_not-resolve/inputTextFile/" + addr;
+  try {
+    const data = fs.readFileSync(addr, "utf-8").toString().trim();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const inputData = inputTextFile("14889.txt");
