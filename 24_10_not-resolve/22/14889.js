cbbcc187bce3ee58d52e1f4829c3b587c5aea0bb
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-10-31 16:21:46 +0900

    Fix: Review not-resolved Question in list 22
    14888 and 14889

--- 24_10_not-resolve/22/14889.js
+++ 24_10_not-resolve/22/14889.js
@@ -11,3 +11,42 @@ function inputTextFile(addr) {
 }
 
 const inputData = inputTextFile("14889.txt");
+let [N, ...ablities] = inputData.split("\r\n");
+N = Number(N);
+ablities = ablities.map((v) => {
+  return v.split(" ").map(Number);
+});
+
+let visited = Array(N).fill(false);
+let min = Number.MAX_SAFE_INTEGER;
+
+function dfs(n, v) {
+  if (n == N / 2) {
+    let [start, link] = [[], []];
+    let [sScore, lScore] = [0, 0];
+
+    for (let i = 0; i < N; i++) {
+      if (visited[i]) start.push(i);
+      else link.push(i);
+    }
+
+    for (let i = 0; i < N / 2; i++) {
+      for (let j = i + 1; j < N / 2; j++) {
+        sScore += ablities[start[i]][start[j]] + ablities[start[j]][start[i]];
+        lScore += ablities[link[i]][link[j]] + ablities[link[j]][link[i]];
+      }
+    }
+
+    min = Math.min(min, Math.abs(sScore - lScore));
+    return;
+  }
+
+  for (let i = v; i < N; i++) {
+    visited[i] = true;
+    dfs(n + 1, i + 1);
+    visited[i] = false;
+  }
+}
+
+dfs(0, 0);
+console.log(`${min}`);
