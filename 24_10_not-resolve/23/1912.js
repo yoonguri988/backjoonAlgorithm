c4c39129c3caff244cb5b15471ce536f73535bef
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-07 17:25:53 +0900

    Feat: Create not-resolved question list by Number 23
    1912 연속합, 1149 RGB거리

---
+++ 24_10_not-resolve/23/1912.js
@@ -0,0 +1,13 @@
+function readTextFile(addr) {
+  const fs = require("fs");
+  if (process.platform == "linux") addr = "/dev/stdin";
+  else addr = "./24_10_not-resolve/inputTextFile/" + addr;
+  try {
+    const data = fs.readFileSync(addr, "utf-8");
+    return data.toString().trim();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+let iData = readTextFile("1912.txt");
