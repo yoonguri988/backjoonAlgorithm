597b1c0ee3ff66103105681ebc604386e87275e2
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-07 23:23:51 +0900

    Fix: Resolve not-resovled question list by Number 23
    문제 풀이 결과 확인 후 해결

--- 24_10_not-resolve/23/1912.js
+++ 24_10_not-resolve/23/1912.js
@@ -11,3 +11,18 @@ function readTextFile(addr) {
 }
 
 let iData = readTextFile("1912.txt");
+const [N, NARR] = iData.split("\r\n").map((v) => {
+  return v.split(" ").map(Number);
+});
+
+const dp = Array(N[0]).fill(0);
+dp[0] = NARR[0];
+
+for (let i = 1; i < N[0]; i++) {
+  if (dp[i - 1] < 0) {
+    dp[i] = NARR[i];
+    continue;
+  }
+  dp[i] = dp[i - 1] + NARR[i];
+}
+console.log(Math.max(...dp));
