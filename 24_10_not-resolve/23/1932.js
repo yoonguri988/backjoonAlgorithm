a501a2f464f29845abc9f5feb2c6fe212ed1c7a9
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-08 22:25:48 +0900

    Fix: Resolve not-resovled question list by Number 23
    2579 시간초과 극복 못해서 해설풀이 봤음
    1932 문제에대한 이해는 했으나 해설풀이를 봤음

--- 24_10_not-resolve/23/1932.js
+++ 24_10_not-resolve/23/1932.js
@@ -11,3 +11,21 @@ function readTextFile(addr) {
 }
 
 let iData = readTextFile("1932.txt");
+const [N, ...TRI_NUMS] = iData
+  .split("\r\n")
+  .map((v) => v.split(" ").map(Number));
+
+let dp = [...TRI_NUMS];
+for (let i = 1; i < N; i++) {
+  for (let j = 0; j < dp[i].length; j++) {
+    if (j == 0) dp[i][j] += dp[i - 1][j];
+    else if (j == dp[i].length - 1) dp[i][j] += dp[i - 1][j - 1];
+    else dp[i][j] += Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+  }
+}
+console.log(Math.max(...dp[N - 1]));
+
+// console.log("----test");
+// console.log(`${N}`);
+// console.log(`${TRI_NUMS.join("\n")}`);
+// console.log(`${dp.join("|")}`);
--- 24_10_not-resolve/23/2579.js
+++ 24_10_not-resolve/23/2579.js
@@ -11,3 +11,16 @@ function readTextFile(addr) {
 }
 
 let iData = readTextFile("2579.txt");
+const [N, ...STAIRS] = iData.split("\r\n").map(Number);
+const dp = Array(N).fill(0);
+dp[0] = STAIRS[0];
+dp[1] = Math.max(STAIRS[0] + STAIRS[1], STAIRS[1]);
+dp[2] = Math.max(STAIRS[0] + STAIRS[2], STAIRS[1] + STAIRS[2]);
+
+for (let i = 3; i < N; i++) {
+  dp[i] = Math.max(
+    dp[i - 3] + STAIRS[i - 1] + STAIRS[i],
+    dp[i - 2] + STAIRS[i]
+  );
+}
+console.log(Math.max(...dp));
